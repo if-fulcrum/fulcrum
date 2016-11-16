4fe77b5032ca7b0004aa62edfd882c0509e0269d
Kenn Herman <kenn@ifsight.com>
2016-11-16 15:24:30 -0800

    fix for checkouts under webroots

--- php/fulcrum.php
+++ php/fulcrum.php
@@ -15,8 +15,8 @@ if (isset($_SERVER['FULCRUM_CONF'])) {
   if (file_exists('/config.json')) {
     $_FULCRUM['conf'] = json_decode(preg_replace('/\\\\\\\\/', '\\', file_get_contents('/config.json')), 1);
   } else if (
-    preg_match("#(.*)/(repos(?:/docroot)?|fulcrum/webroots)/([^/]+).*#", getcwd(), $matches) AND
-    $json = preg_replace("#(.*)/repos(?:/docroot)?/([^/]+).*#", "$1/fulcrum/conf/$2.json", getcwd()) AND
+    preg_match(          "#(.*)/(?:repos(?:/docroot)?|fulcrum/webroots)/([^/]+).*#", getcwd(), $matches) AND
+    $json = preg_replace("#(.*)/(?:repos(?:/docroot)?|fulcrum/webroots)/([^/]+).*#", "$1/fulcrum/conf/$2.json", getcwd()) AND
     file_exists($json)
   ) {
     global $base_url;
