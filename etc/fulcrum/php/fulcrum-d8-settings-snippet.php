70a0256c8e425da4e4fc305d2616b58ddd9becc4
Kenn Herman <kenn@ifsight.com>
2016-11-25 18:56:47 -0800

    show snippets

---
+++ etc/fulcrum/php/fulcrum-d8-settings-snippet.php
@@ -0,0 +1,6 @@
+foreach (array('/fulcrum/fulcrum.php', "{$_SERVER['HOME']}/fulcrum/etc/fulcrum/php/fulcrum.php", "{$_SERVER['HOME']}/fulcrum/php/fulcrum.php") as $f) {
+  if (is_file($f) && (include $f) && isset($_FULCRUM)) {
+    fulcrum_cfg('pre', $_FULCRUM['conf'], $settings, $databases);
+    break;
+  }
+}
--- etc/fulcrum/php/fulcrum-settings-simple.php
+++
@@ -1,4 +0,0 @@
-
-if ($ff = '/fulcrum/fulcrum.php' AND file_exists($ff) AND (include $ff) AND isset($_FULCRUM)) {
-  fulcrum_cfg('pre', $_FULCRUM['conf']);
-}
